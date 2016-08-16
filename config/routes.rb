b782a033caeb4730a9eeb2051bade5d9705e58c2
dianacamacho <dcam1901@yahoo.com>
2016-08-16 02:03:24 -0500

    home page personal info edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/' => 'pages#home'
+  get '/resume' => 'pages#resume'
+  get '/portfolio' => 'pages#portfolio'
+  get '/blog' => 'pages#blog'
+  get '/interests' => 'pages#interests'
+  get '/contact' => 'pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
