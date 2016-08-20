3901f6eadd868c49ce8b83031ad12ebeccb9f74c
dianacamacho <dcam1901@yahoo.com>
2016-08-20 02:11:02 -0500

    renamed devise sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
+  devise_for :users, skip: [:registrations, :sessions]
   as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    get '/sign_in' => 'devise/sessions#new', :as => 'new_user_session'
+    post '/sign_in' => 'devise/sessions#create', :as => 'user_session'
+    get '/sign_out' => 'devise/sessions#destroy', :as => 'destroy_user_session'
+    get '/users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put '/users(.:format)' => 'devise/registrations#update', as: 'user_registration'
     patch '/users(.:format)' => 'devise/registrations#update'
   end
