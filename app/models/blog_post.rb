d3933c262952cfa1c73f34db811c430d8bf8e1e7
dianacamacho <dcam1901@yahoo.com>
2016-08-20 02:55:17 -0500

    blog post author and friendly_created_at methods

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,12 @@
 class BlogPost < ApplicationRecord
+  belongs_to :user
   validates :title, :text, presence: true
+
+  def author
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %d, %Y")
+  end
 end
