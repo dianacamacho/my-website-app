02ade6d53cd3733d0bea5f65f88e26ad7ee5b4fc
dianacamacho <dcam1901@yahoo.com>
2016-08-27 04:47:43 -0500

    project show screencap carousel

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :technologies, dependent: :destroy
   belongs_to :user
 
-  validates :name, :description, presence: true
+  validates :name, :description, :image_1, presence: true
 
   def author
     "#{user.first_name} #{user.last_name}"
@@ -14,6 +14,6 @@ class Project < ApplicationRecord
 
   def images
     project_images = [image_1, image_2, image_3, image_4]
-    project_images.reject! { |photo| photo == "" }
+    project_images.reject { |photo| photo == "" || photo == nil }
   end
 end
