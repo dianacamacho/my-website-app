4711ebf982b9a1da1acc025a134c7044f390a60e
dianacamacho <dcam1901@yahoo.com>
2016-08-27 04:19:58 -0500

    project show, bootstrap gem for js

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,19 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :technologies, dependent: :destroy
+  belongs_to :user
+
+  validates :name, :description, presence: true
+
+  def author
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %d, %Y")
+  end
+
+  def images
+    project_images = [image_1, image_2, image_3, image_4]
+    project_images.reject! { |photo| photo == "" }
+  end
 end
